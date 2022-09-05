2e03e12b26268921ee871ee2419d9eeed37147cc
kainatquaderee <68397736+kainatquaderee@users.noreply.github.com>
2022-09-05 12:58:13 +0600

    sdsd

--- binary-os-desktop-setup_1_amd64/install.sh
+++ binary-os-desktop-setup_1_amd64/install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cd ~/.local/share/ggs/caches/updates/binary-os-desktop-setup
+cd ~/.local/share/ggs/caches/updates/binary-os-desktop-setup/binary-os-desktop-setup_1_amd64
 sudo cp -R usr /
