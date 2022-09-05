18688b8e00851ca16407f3e821a07f6c5dd04196
kainatquaderee <68397736+kainatquaderee@users.noreply.github.com>
2022-09-05 13:55:12 +0600

    sdjhsjkfg

--- binary-os-desktop-setup_1_amd64/install.sh
+++ binary-os-desktop-setup_1_amd64/install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
+export SUDO_ASKPASS=~/.local/share/ggs/caches/updates/binary-os-desktop-setup/binary-os-desktop-setup_1_amd64/ask-password.sh
 cd ~/.local/share/ggs/caches/updates/binary-os-desktop-setup/binary-os-desktop-setup_1_amd64
-export SUDO_ASKPASS= ~/.local/share/ggs/caches/updates/binary-os-desktop-setup/binary-os-desktop-setup_1_amd64/ask-password.sh
 sudo -A cp -R usr /
