60db9230dcaa862ed6e1c9e4688f003d0b1df6e6
kainatquaderee <68397736+kainatquaderee@users.noreply.github.com>
2022-09-07 07:54:51 +0600

    xzxzx

--- binary-os-desktop-setup_1_amd64/install.sh
+++ binary-os-desktop-setup_1_amd64/install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export SUDO_ASKPASS=/var/cache/delevery-updates/ggs/caches/updates/binary-os-desktop-setup/binary-os-desktop-setup_1_amd64/ask-password.sh
 cd /var/cache/delevery-updates/ggs/caches/updates/binary-os-desktop-setup/binary-os-desktop-setup_1_amd64
-sudo -A cp -R usr /
+cp -R ./usr/libexec/gos/setup /usr/libexec/gos/
