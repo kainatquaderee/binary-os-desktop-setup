03246cffaaa3beac6879320029d7cd2ae20c1c70
kainatquaderee <68397736+kainatquaderee@users.noreply.github.com>
2022-09-18 08:45:17 +0600

    sddsd

--- binary-os-desktop-setup_1_amd64/usr/libexec/gos/setup/common-setup/common-setup.sh
+++ binary-os-desktop-setup_1_amd64/usr/libexec/gos/setup/common-setup/common-setup.sh
@@ -12,11 +12,11 @@ export SUDO_ASKPASS= /usr/libexec/gos/setup/common-setup/ask-password.sh
 sudo -A add-apt-repository universe -y
 sudo -A add-apt-repository restricted -y
 sudo -A add-apt-repository multiverse -y
-apt-get install gimp
+apt-get install gimp -y
 apt-get install playonlinux -y
-add-apt-repository ppa:kdenlive/kdenlive-stable
-apt-get update
+add-apt-repository ppa:kdenlive/kdenlive-stable -y
+apt-get update -y
 apt-get install kdenlive -y
 apt-get install synaptic -y
-apt-get install yad
-flatpak install flathub org.blender.Blender
+apt-get install yad -y
+flatpak install flathub org.blender.Blender -y
